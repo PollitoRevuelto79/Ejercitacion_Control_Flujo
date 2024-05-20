4f1cdbccda2084ddf993f276be3025e1dd559607
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-05-20 12:40:28 -0300

    TP3, se termino la estructura basica del programa

--- assets/Tareas.js
+++ assets/Tareas.js
@@ -1,25 +1,41 @@
-let Lista = [];
+let lista = [];
 
-function Editar(params) {
-    
+function editar(TareaUno, TareaDos) {
+    if (lista.includes(TareaUno)) {
+        let posicion = lista.indexOf(TareaUno);
+    if (posicion !== -1) {
+        lista.splice(posicion, 1, TareaDos);
+    } else {
+        alert(`Elemento "${TareaDos}" no encontrado en la lista.`);
+    }
+    }
 }
 
-let Options
-while (Options != 0) {
-    Options = parseInt(prompt('Ingrese una opción.\n1.Ver lista de tareas.\n2.Crear nueva tarea.\n3.Editar tarea.\n4.Eliminar tarea.\n0.Salir'));
-switch (Options) {
+function eliminar(materia) {
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i] === materia) {
+            lista.splice(i , 1)
+        }
+    }
+}
+
+let options
+while (options != 0) {
+    options = parseInt(prompt('Ingrese una opción.\n1.Ver lista de tareas.\n2.Crear nueva tarea.\n3.Editar tarea.\n4.Eliminar tarea.\n0.Salir'));
+switch (options) {
     case 1:
-        alert(Lista)
+        alert(lista)
         break;
     case 2:
-        Lista.push(prompt('Escriba el nombre de la material.'))
+        lista.push(prompt('Escriba el nombre de la materia.'))
         break
     case 3:
-        for (let i = 0; i < Lista.length; i++) {
-            const element = Lista[i];}
-            console.log(i)
+        let TareaUno = prompt(`Ingrese nombre de la tarea que desea editar.\n${lista}`)
+        let TareaDos = prompt('Ingresar el nuevo nombre de la tarea.')
+        editar(TareaUno, TareaDos)
         break;
     case 4:
+        eliminar(prompt(`Ingrese nombre de la tarea que desea eliminar.\n${lista}`))
         break;
 }
 }
